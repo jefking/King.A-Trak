2ee131c4c3a1ee45be8b808ec628e664fb19d4a4
Jef King <jef@agilebusinesscloud.com>
2011-12-28 18:56:20 -0800

    Program fails when exception occurs MD5 of file stored Snapshot is created when file contents changes. Upload only occurs when a new file or changed file is processed

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
     using System.Diagnostics;
     using System.IO;
     using System.Linq;
+    using System.Security.Cryptography;
     using System.Security.Permissions;
     using System.Threading.Tasks;
     using Microsoft.Win32;
@@ -16,6 +17,13 @@
     /// </summary>
     class Program
     {
+        #region Members
+        /// <summary>
+        /// MD5 Key for Metadata
+        /// </summary>
+        private const string MD5MetadataKey = "MD5";
+        #endregion
+
         /// <summary>
         /// Program Main Entry
         /// </summary>
@@ -52,7 +60,7 @@
                 }
                 catch (Exception ex)
                 {
-                    Trace.WriteLine(ex.Message);
+                    Trace.Fail(ex.Message);
                 }
             }
 
@@ -75,17 +83,55 @@
             var files = GetFiles(folder, new List<string>());
             Parallel.ForEach<string>(files, (file, state) =>
             {
-                Trace.WriteLine(string.Format("Uploading file: '{0}'.", file));
+                Trace.WriteLine(string.Format("Processing file: '{0}'.", file));
 
-                var objId = file.Replace(path, string.Empty).ToLowerInvariant();
+                var objId = file.Replace(path, string.Empty);
                 var blob = container.GetBlobReference(objId);
-                blob.UploadByteArray(File.ReadAllBytes(file));
+                bool exists = false;
 
-                var contentType = ContentType(file);
-                if (!string.IsNullOrWhiteSpace(contentType))
+                try
                 {
-                    blob.Properties.ContentType = contentType;
-                    blob.SetProperties();
+                    blob.FetchAttributes();
+                    exists = true;
+                }
+                catch (StorageClientException)
+                {
+                    Trace.WriteLine(string.Format("Uploading new file: '{0}'.", file));
+                }
+
+                var fileContents = File.ReadAllBytes(file);
+                string md5 = null;
+                using (var md5Hash = MD5.Create())
+                {
+                    var data = md5Hash.ComputeHash(fileContents);
+                    md5 = System.Convert.ToBase64String(data);
+                }
+
+                if (!exists || blob.Metadata[MD5MetadataKey] != md5)
+                {
+                    if (!string.IsNullOrWhiteSpace(blob.Properties.ContentType))
+                    {
+                        blob.CreateSnapshot();
+
+                        Trace.WriteLine(string.Format("Created snapshot of blob: '{0}'.", blob.Uri));
+                    }
+                    else
+                    {
+                        blob.Properties.ContentType = ContentType(file);
+                    }
+
+                    // Currently there is a bug in the library that this isn't being stored or retrieved properly, this will be compatible when the new library comes out
+                    blob.Properties.ContentMD5 = md5;
+                    blob.UploadByteArray(fileContents);
+
+                    blob.Metadata[MD5MetadataKey] = md5;
+                    blob.SetMetadata();
+
+                    Trace.WriteLine(string.Format("Uploaded file: '{0}'.", file));
+                }
+                else
+                {
+                    Trace.WriteLine(string.Format("File '{0}' already exists at '{1}', upload avoided.", file, blob.Uri));
                 }
             });
         }
