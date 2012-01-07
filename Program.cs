2bd650b314682adb65ffa3a0d18e60f9b79344ef
Jef King <jef@agilebusinesscloud.com>
2012-01-06 21:24:36 -0800

    A-Trak rename assembly version update (for breaking change)

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿namespace Abc.Atrack
+﻿namespace Abc.Atrak
 {
     using System;
     using System.Collections.Generic;
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,15 +2,15 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("A-Track Windows Azure Blob Uploader")]
+[assembly: AssemblyTitle("A-Trak Windows Azure Blob Uploader")]
 [assembly: AssemblyDescription("Command line tool for uploading files to Windows Azure blob storage.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Agile Business Cloud Solutions Ltd.")]
-[assembly: AssemblyProduct("A-Track")]
+[assembly: AssemblyProduct("A-Trak")]
 [assembly: AssemblyCopyright("Copyright © Agile Business Cloud Solutions Ltd. 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("a4b17f6c-7cba-4b4b-9826-ec4cafc828ae")]
 [assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
