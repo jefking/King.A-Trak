7245e0e8d7c4ba5ca95d931f972a0ba0a9516635
Jef King <jamieking80@gmail.com>
2014-06-16 20:16:56 -0700

    test classes

---
+++ King.ATrak.Test/ContentTypesTests.cs
@@ -0,0 +1,14 @@
+﻿namespace King.ATrak.Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [TestClass]
+    public class ContentTypesTests
+    {
+    }
+}
