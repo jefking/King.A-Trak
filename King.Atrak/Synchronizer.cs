b71534f4ad2b43d68b72cac39a949054f2b78c83
Jef King <jef@agilebusinesscloud.com>
2014-10-25 12:16:11 -0700

    Sync

--- King.Atrak/Synchronizer.cs
+++ King.Atrak/Synchronizer.cs
@@ -1,5 +1,6 @@
 ﻿namespace King.ATrak
 {
+    using King.ATrak.Models;
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -8,5 +9,30 @@
 
     public class Synchronizer
     {
+        #region Constructors
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="config">Configuration Values</param>
+        public Synchronizer(IConfigValues config)
+        {
+            if (null == config)
+            {
+                throw new ArgumentNullException("config");
+            }
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Run Synchronization
+        /// </summary>
+        /// <param name="direction">Direction</param>
+        /// <returns>Task</returns>
+        public virtual async Task Run(Direction direction)
+        {
+            await new TaskFactory().StartNew(() => { });
+        }
+        #endregion
     }
 }
