604fe19f8e6f31176faf2a03f2068bb9700c7ceb
Jef King <jef@agilebusinesscloud.com>
2014-10-25 16:07:20 -0700

    Unit test Model

--- King.ATrak.Test/Models/ConfigValuesTests.cs
+++ King.ATrak.Test/Models/ConfigValuesTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace King.ATrak.Test.Models
 {
+    using King.ATrak.Models;
     using NUnit.Framework;
     using System;
     using System.Collections.Generic;
@@ -10,5 +11,88 @@
     [TestFixture]
     public class ConfigValuesTests
     {
+        [Test]
+        public void Constructor()
+        {
+            new ConfigValues();
+        }
+
+        [Test]
+        public void IsIConfigValues()
+        {
+            Assert.IsNotNull(new ConfigValues() as IConfigValues);
+        }
+
+        [Test]
+        public void CreateSnapshot()
+        {
+            var expected = true;
+            var c = new ConfigValues
+            {
+                CreateSnapshot = expected,
+            };
+
+            Assert.AreEqual(expected, c.CreateSnapshot);
+        }
+
+        [Test]
+        public void CacheControl()
+        {
+            var expected = Guid.NewGuid().ToString();
+            var c = new ConfigValues
+            {
+                CacheControl = expected,
+            };
+
+            Assert.AreEqual(expected, c.CacheControl);
+        }
+
+        [Test]
+        public void Folder()
+        {
+            var expected = Guid.NewGuid().ToString();
+            var c = new ConfigValues
+            {
+                Folder = expected,
+            };
+
+            Assert.AreEqual(expected, c.Folder);
+        }
+
+        [Test]
+        public void ContainerName()
+        {
+            var expected = Guid.NewGuid().ToString();
+            var c = new ConfigValues
+            {
+                ContainerName = expected,
+            };
+
+            Assert.AreEqual(expected, c.ContainerName);
+        }
+
+        [Test]
+        public void ConnectionString()
+        {
+            var expected = Guid.NewGuid().ToString();
+            var c = new ConfigValues
+            {
+                ConnectionString = expected,
+            };
+
+            Assert.AreEqual(expected, c.ConnectionString);
+        }
+
+        [Test]
+        public void SyncDirection()
+        {
+            var expected = Direction.FolderToBlob;
+            var c = new ConfigValues
+            {
+                SyncDirection = expected,
+            };
+
+            Assert.AreEqual(expected, c.SyncDirection);
+        }
     }
 }
