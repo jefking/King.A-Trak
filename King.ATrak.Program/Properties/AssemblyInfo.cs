a56232e8efd05ad120b6dc266823c2dc0c619d31
Jef King <jef@agilebusinesscloud.com>
2014-10-25 15:30:29 -0700

    info

--- King.ATrak.Program/Properties/AssemblyInfo.cs
+++ King.ATrak.Program/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("A-Trak, synchronize Azure, Amazon and Windows")]
-[assembly: AssemblyDescription("Command line tool for synchronizing Files between Windows, Azure and Amazon.")]
+[assembly: AssemblyTitle("Move content between Windows and Azure.")]
+[assembly: AssemblyDescription("Move content between Windows and Azure.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jef King")]
 [assembly: AssemblyProduct("A-Trak")]
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("b6028539-727d-4a26-a882-d21ea715c36b")]
-[assembly: AssemblyVersion("1.0.3.7")]
-[assembly: AssemblyFileVersion("1.0.3.7")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
