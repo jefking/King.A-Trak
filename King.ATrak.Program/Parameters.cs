436fa08e0588c33d67362f81e7086409be180243
Jef King <jef@agilebusinesscloud.com>
2014-10-25 15:29:53 -0700

    Program Re-work

--- King.ATrak.Program/Parameters.cs
+++ King.ATrak.Program/Parameters.cs
@@ -1,8 +1,10 @@
 ﻿namespace King.ATrak
 {
-    using Amazon;
+    using King.ATrak.Models;
     using Microsoft.WindowsAzure.Storage;
     using System;
+    using System.Linq;
+    using System.Collections.Generic;
     using System.Configuration;
     using System.Diagnostics;
     using System.IO;
@@ -17,41 +19,25 @@
         /// <summary>
         /// Arguments
         /// </summary>
-        protected readonly string[] arguments;
-
-        /// <summary>
-        /// Parameters Regex Statement
-        /// </summary>
-        private const string parametersRegexStatement = "[/](?<action>From|To)[ ](?<arguments>.[^/]*)";
-
-        /// <summary>
-        /// Values Regex Statement
-        /// </summary>
-        private static readonly string valuesRegexStatement = "[" + quote + "']*(?<value>.[^" + quote + "']*)[" + quote + "' ]*";
-
-        /// <summary>
-        /// Quote
-        /// </summary>
-        private const char quote = '\"';
-
-        /// <summary>
-        /// Backslash
-        /// </summary>
-        private const char backslash = '\\';
-
-        /// <summary>
-        /// Directory Match Regex Statement
-        /// </summary>
-        private const string directoryMatch = @"^(([a-zA-Z]:)|(\\{2}\w+)\$?)(\\(\w[\w ]*))+";
+        protected readonly IReadOnlyList<string> arguments;
         #endregion
 
         #region Constructors
         /// <summary>
-        /// Parameters
+        /// Default Constructor
         /// </summary>
         /// <param name="arguments">Arguments</param>
-        public Parameters(string[] arguments)
+        public Parameters(IReadOnlyList<string> arguments)
         {
+            if (null == arguments)
+            {
+                throw new ArgumentNullException("arguments");
+            }
+            if (!arguments.Any() || arguments.Count() != 2)
+            {
+                throw new ArgumentException("Invalid parameter count.");
+            }
+
             this.arguments = arguments;
         }
         #endregion
@@ -61,167 +47,12 @@
         /// Process Settings for Facotry
         /// </summary>
         /// <returns>Storage Factory</returns>
-        public virtual StorageFactory Process()
+        public virtual IConfigValues Process()
         {
-            var factory = new StorageFactory();
-            var from = ConfigurationManager.AppSettings["From"];
-            var to = ConfigurationManager.AppSettings["To"];
-
-            if (null != arguments)
+            return new ConfigValues
             {
-                var appendingFrom = true;
-                var toParse = string.Join(" ", arguments);
-                foreach (var arg in arguments)
-                {
-                    var val = arg.ToLowerInvariant();
-                    if (val == "/from")
-                    {
-                        appendingFrom = true;
-                    }
-                    else if (val == "/to")
-                    {
-                        appendingFrom = false;
-                    }
-                    else if (appendingFrom)
-                    {
-                        from += string.Format("\"{0}\"", arg);
-                    }
-                    else
-                    {
-                        to += string.Format("\"{0}\"", arg);
-                    }
-                }
-            }
 
-            if (string.IsNullOrWhiteSpace(from))
-            {
-                throw new InvalidOperationException("Specify where the data is coming from '/From' in arguments, or in AppSettings.");
-            }
-            else if (string.IsNullOrWhiteSpace(to))
-            {
-                throw new InvalidOperationException("Specify where the data is going to '/To' in arguments, or in AppSettings.");
-            }
-            else
-            {
-                var fromValues = Regex.Matches(from, valuesRegexStatement, RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
-
-                switch (fromValues.Count)
-                {
-                    case 1:
-                        var directory = fromValues[0].Groups["value"].Value;
-                        if (Directory.Exists(directory))
-                        {
-                            Trace.WriteLine(string.Format("Synchronizing from folder: '{0}'", directory));
-
-                            factory.AddDirectory(directory);
-                        }
-                        else
-                        {
-                            throw new InvalidOperationException("Directory does not exist.");
-                        }
-                        break;
-                    case 2:
-                        var accountArgument = fromValues[0].Groups["value"].Value;
-                        var container = fromValues[1].Groups["value"].Value;
-                        CloudStorageAccount account;
-                        if (accountArgument == "UseDevelopmentStorage=true")
-                        {
-                            account = CloudStorageAccount.DevelopmentStorageAccount;
-                        }
-                        else if (!CloudStorageAccount.TryParse(accountArgument, out account))
-                        {
-                            account = null;
-                        }
-
-                        if (null != account)
-                        {
-                            if (!string.IsNullOrWhiteSpace(container))
-                            {
-                                Trace.WriteLine(string.Format("Synchronizing container: '{0}'", container));
-
-                                factory.AddContainer(account, container);
-                            }
-                            else
-                            {
-                                Trace.WriteLine("Storage Account Credentials must be coupled with container; please specify a container to synchronize to.");
-                            }
-                        }
-                        else
-                        {
-                            Trace.WriteLine("Storage Account Credentials is in invalid format.");
-                        }
-                        break;
-                    case 3:
-                        var accessKey = fromValues[0].Groups["value"].Value;
-                        var secretAccessKey = fromValues[1].Groups["value"].Value;
-                        var bucket = fromValues[2].Groups["value"].Value;
-                        var client = AWSClientFactory.CreateAmazonS3Client(accessKey, secretAccessKey);
-                        factory.AddBucket(client, bucket);
-                        break;
-                    default:
-                        throw new InvalidOperationException(string.Format("Unknown parameters: '{0}'", from));
-                }
-
-                var toValues = Regex.Matches(to, valuesRegexStatement, RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
-
-                switch (toValues.Count)
-                {
-                    case 1:
-                        if (Regex.IsMatch(toValues[0].Groups["value"].Value, directoryMatch))
-                        {
-                            Trace.WriteLine(string.Format("Synchronizing folder: '{0}'", toValues[0].Groups["value"].Value));
-
-                            factory.AddDirectory(toValues[0].Groups["value"].Value);
-                        }
-                        else
-                        {
-                            throw new InvalidOperationException("Directory is invalid.");
-                        }
-                        break;
-                    case 2:
-                        var accountArgument = toValues[0].Groups["value"].Value;
-                        var container = toValues[1].Groups["value"].Value;
-                        CloudStorageAccount account;
-                        if (accountArgument == "UseDevelopmentStorage=true")
-                        {
-                            account = CloudStorageAccount.DevelopmentStorageAccount;
-                        }
-                        else if (!CloudStorageAccount.TryParse(accountArgument, out account))
-                        {
-                            account = null;
-                        }
-
-                        if (null != account)
-                        {
-                            if (!string.IsNullOrWhiteSpace(container))
-                            {
-                                Trace.WriteLine(string.Format("Synchronizing container: '{0}'", container));
-
-                                factory.AddContainer(account, container);
-                            }
-                            else
-                            {
-                                Trace.WriteLine("Storage Account Credentials must be coupled with container; please specify a container to synchronize to.");
-                            }
-                        }
-                        else
-                        {
-                            Trace.WriteLine("Storage Account Credentials is in invalid format.");
-                        }
-                        break;
-                    case 3:
-                        var accessKey = toValues[0].Groups["value"].Value;
-                        var secretAccessKey = toValues[1].Groups["value"].Value;
-                        var bucket = toValues[2].Groups["value"].Value;
-                        var client = AWSClientFactory.CreateAmazonS3Client(accessKey, secretAccessKey);
-                        factory.AddBucket(client, bucket);
-                        break;
-                    default:
-                        throw new InvalidOperationException(string.Format("Unknown parameters: '{0}'", to));
-                }
-            }
-
-            return factory;
+            };
         }
         #endregion
     }
--- King.ATrak.Program/Program.cs
+++ King.ATrak.Program/Program.cs
@@ -1,10 +1,8 @@
 ﻿namespace King.ATrak
 {
-    using Microsoft.WindowsAzure;
     using System;
-    using System.Collections.Generic;
     using System.Diagnostics;
-    using System.Threading.Tasks;
+    using System.Threading;
 
     /// <summary>
     /// A-Trak synchronizer, for Azure, folders and Amazon
@@ -18,19 +16,21 @@
         /// <param name="args">Program Arguments</param>
         public static void Main(string[] args)
         {
+            Trace.TraceInformation("Starting...");
+
             try
             {
                 var parameters = new Parameters(args);
-                var factory = parameters.Process();
+                var config = parameters.Process();
 
-                if (factory.Validate())
-                {
-                    Synchronize(factory);
-                }
-                else
-                {
-                    Trace.WriteLine("Failed to initialize; invalid parameters.");
-                }
+                Trace.TraceInformation("Connection String: '{0}'{3}{3}Container: '{1}'{3}Folder: '{2}'{3}"
+                    , config.ConnectionString
+                    , config.ContainerName
+                    , config.Folder
+                    , Environment.NewLine);
+
+                var sync = new Synchronizer(config);
+                sync.Run(config.SyncDirection).Wait();
             }
             catch (Exception ex)
             {
@@ -38,79 +38,8 @@
             }
 
             Trace.TraceInformation("Completed.");
-        }
 
-        /// <summary>
-        /// Synchronize Contents
-        /// </summary>
-        /// <param name="factory">Storage Factory</param>
-        private static void Synchronize(StorageFactory factory)
-        {
-            Program.Push(factory);
-
-            var delete = false;
-            bool.TryParse(CloudConfigurationManager.GetSetting("Synchronize"), out delete);
-
-            if (delete)
-            {
-                Trace.TraceInformation("Deleting items which are not in source.");
-
-                Program.Delete(factory);
-            }
-        }
-
-        /// <summary>
-        /// Push data from source to destination
-        /// </summary>
-        /// <param name="factory">Storage Factory</param>
-        private static void Push(StorageFactory factory)
-        {
-            Parallel.ForEach<IStorageItem>(factory.Source(), (from, state) =>
-            {
-                Trace.TraceInformation("Processing file: '{0}'.", from);
-
-                var to = factory.To(from);
-                var exists = to.Exists();
-                if (!exists)
-                {
-                    Trace.TraceInformation("Synchronizing new file: '{0}'.", from);
-                }
-
-                if (!exists || to.MD5 != from.MD5 || to.MD5 == null)
-                {
-                    to.Save(from, exists);
-
-                    Trace.TraceInformation("Synchronizing file: '{0}'.", from);
-                }
-                else
-                {
-                    Trace.TraceInformation("File '{0}' already exists at '{1}', synchronization avoided.", from.Path, to.Path);
-                }
-
-                to = null;
-                from = null;
-            });
-        }
-
-        /// <summary>
-        /// Delete items from destination
-        /// </summary>
-        /// <param name="factory">Storage Factory</param>
-        private static void Delete(StorageFactory factory)
-        {
-            var items = new HashSet<string>();
-            foreach (var item in factory.Source())
-            {
-                items.Add(item.RelativePath);
-            }
-
-            Parallel.ForEach<IStorageItem>(factory.Destination(), (item, state) =>
-            {
-                if (!items.Contains(item.RelativePath))
-                {
-                    item.Delete();
-                }
-            });
+            Thread.Sleep(2000);
         }
         #endregion
     }
