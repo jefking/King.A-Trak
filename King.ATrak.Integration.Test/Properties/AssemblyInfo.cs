70166f44c12f0fff198d76ace92b5dfb4ec090ad
Jef King <jef@agilebusinesscloud.com>
2014-10-25 14:07:31 -0700

    Integration Tests

---
+++ King.ATrak.Integration.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("King.ATrak.Integration.Test")]
+[assembly: AssemblyDescription("Integration Tests for A-Trak")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("King.ATrak.Integration.Test")]
+[assembly: AssemblyCopyright("Copyright © Jef King 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("019b0051-9ff7-4f10-b9f1-99fc7649e713")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
