8db3bd48ebd8fdb77f55d0bbc4107816ad271f73
Jef King <jef@agilebusinesscloud.com>
2014-10-25 18:04:27 -0700

    Constructors covered

--- King.ATrak.Test/SynchronizerTests.cs
+++ King.ATrak.Test/SynchronizerTests.cs
@@ -1,14 +1,46 @@
 ﻿namespace King.ATrak.Test
 {
+    using King.ATrak.Models;
     using NUnit.Framework;
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     [TestFixture]
     public class SynchronizerTests
     {
+        [Test]
+        public void Constructor()
+        {
+            var config = new ConfigValues
+            {
+                ConnectionString = "UseDevelopmentStorage=true;",
+                ContainerName = "test",
+                Folder = "C:\\happy",
+                SyncDirection = Direction.FolderToBlob,
+            };
+
+            new Synchronizer(config);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorConfigNull()
+        {
+            new Synchronizer(null);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorDirectionUnknown()
+        {
+            var config = new ConfigValues
+            {
+                ConnectionString = "UseDevelopmentStorage=true;",
+                ContainerName = "test",
+                Folder = "C:\\happy",
+                SyncDirection = Direction.Unknown,
+            };
+
+            new Synchronizer(config);
+        }
     }
 }
