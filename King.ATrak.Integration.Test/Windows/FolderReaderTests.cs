44a213e36b1944c9487d2a1d3e4417ac42425187
Jef King <jef@agilebusinesscloud.com>
2014-10-26 17:36:00 -0700

    Reader Test

--- King.ATrak.Integration.Test/Windows/FolderReaderTests.cs
+++ King.ATrak.Integration.Test/Windows/FolderReaderTests.cs
@@ -1,14 +1,39 @@
 ﻿namespace King.ATrak.Integration.Test.Windows
 {
+    using King.ATrak.Windows;
     using NUnit.Framework;
     using System;
-    using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     [TestFixture]
     public class FolderReaderTests
     {
+        [Test]
+        public void List()
+        {
+            var random = new Random();
+            var count = random.Next(1, 25);
+            var root = string.Format("{0}\\{1}", Environment.CurrentDirectory, Guid.NewGuid());
+            Directory.CreateDirectory(root);
+
+            for (var i = 0; i < count; i++)
+            {
+                var bytes = new byte[8];
+                random.NextBytes(bytes);
+
+                var folder = string.Format("{0}\\{1}", root, Guid.NewGuid());
+                Directory.CreateDirectory(folder);
+
+                var file = string.Format("{0}\\{1}.bin", folder, Guid.NewGuid());
+                File.WriteAllBytes(file, bytes);
+            }
+
+            var reader = new FolderReader(root);
+            var results = reader.List();
+
+            Assert.IsNotNull(results);
+            Assert.AreEqual(count, results.Count());
+        }
     }
 }
