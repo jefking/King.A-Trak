1c4c107251fbf096b0bd6bea90a8200e74742911
Jef King <jef@agilebusinesscloud.com>
2014-10-27 18:02:40 -0700

    All tested up

--- King.ATrak.Integration.Test/SynchronizerTests.cs
+++ King.ATrak.Integration.Test/SynchronizerTests.cs
@@ -128,47 +128,109 @@
         [Test]
         public async Task FolderToFolder()
         {
-            Assert.Inconclusive();
+            var from = string.Format("{0}\\{1}", Environment.CurrentDirectory, Guid.NewGuid());
+            Directory.CreateDirectory(from);
+            var to = string.Format("{0}\\{1}", Environment.CurrentDirectory, Guid.NewGuid());
+
+            var random = new Random();
+            var count = random.Next(1, 25);
+            var toValidate = new List<Validation>(count);
+            for (var i = 0; i < count; i++)
+            {
+                var v = new Validation
+                {
+                    Data = new byte[64],
+                    FileName = string.Format("{0}.{1}", Guid.NewGuid(), i),
+                };
+
+                var bytes = new byte[64];
+                random.NextBytes(v.Data);
+
+                File.WriteAllBytes(string.Format("{0}\\{1}", from, v.FileName), v.Data);
+
+                toValidate.Add(v);
+            }
 
             var config = new ConfigValues
             {
                 Source = new DataSource
                 {
-                    Folder = "C:\\From",
+                    Folder = from,
                 },
                 Destination = new DataSource
                 {
-                    Folder = "C:\\To",
+                    Folder = to,
                 },
                 Direction = Direction.FolderToFolder,
             };
 
             var s = new Synchronizer(config);
             await s.Run();
+            
+            // Validate
+            foreach (var v in toValidate)
+            {
+                var data = File.ReadAllBytes(string.Format("{0}\\{1}", to, v.FileName));
+                Assert.AreEqual(v.Data, data);
+            }
         }
 
         [Test]
         public async Task BlobToBlob()
         {
-            Assert.Inconclusive();
+            var containerName = 'a' + Guid.NewGuid().ToString().Replace("-", "");
+            var destContainerName = 'b' + Guid.NewGuid().ToString().Replace("-", "");
+            var from = new Container(containerName, ConnectionString);
+            await from.CreateIfNotExists();
+
+            var random = new Random();
+            var count = random.Next(1, 25);
+            var toValidate = new List<Validation>(count);
+            for (var i = 0; i < count; i++)
+            {
+                var v = new Validation
+                {
+                    Data = new byte[64],
+                    FileName = string.Format("{0}.{1}", Guid.NewGuid(), i),
+                };
+
+                var bytes = new byte[64];
+                random.NextBytes(v.Data);
+
+                await from.Save(v.FileName, v.Data);
+
+                toValidate.Add(v);
+            }
 
             var config = new ConfigValues
             {
                 Source = new DataSource
                 {
                     ConnectionString = ConnectionString,
-                    ContainerName = "",
+                    ContainerName = containerName,
                 },
                 Destination = new DataSource
                 {
                     ConnectionString = ConnectionString,
-                    ContainerName = "",
+                    ContainerName = destContainerName,
                 },
                 Direction = Direction.BlobToBlob,
             };
 
             var s = new Synchronizer(config);
             await s.Run();
+
+            //Validate
+            var to = new Container(destContainerName, ConnectionString);
+            foreach (var v in toValidate)
+            {
+                var data = await to.Get(v.FileName);
+                Assert.AreEqual(v.Data, data);
+            }
+
+            //Clean-Up
+            await from.Delete();
+            await to.Delete();
         }
     }
 }
