0bfe0a785cdafdf28f7b748f27cf8b3c15c3245b
Jef King <jef@agilebusinesscloud.com>
2014-11-01 09:14:36 -0700

    Test version

--- King.ATrak.Integration.Test/Properties/AssemblyInfo.cs
+++ King.ATrak.Integration.Test/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("019b0051-9ff7-4f10-b9f1-99fc7649e713")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
