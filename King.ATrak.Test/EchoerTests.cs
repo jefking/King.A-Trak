38b4169277318a094ebcf8f317ef6fe66a31983f
Jef King <jef@agilebusinesscloud.com>
2014-11-02 20:57:39 -0800

    Echoer, unit tested

---
+++ King.ATrak.Test/EchoerTests.cs
@@ -0,0 +1,99 @@
+﻿namespace King.ATrak.Test
+{
+    using NSubstitute;
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+
+    [TestFixture]
+    public class EchoerTests
+    {
+        [Test]
+        public void Constructor()
+        {
+            var destination = Substitute.For<IDataLister>();
+            new Echoer(destination);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorDestinationNull()
+        {
+            new Echoer(null);
+        }
+
+        [Test]
+        public void IsIEchoer()
+        {
+            var destination = Substitute.For<IDataLister>();
+            Assert.IsNotNull(new Echoer(destination) as IEchoer);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CleanDestinationSourceItemsNull()
+        {
+            var destination = Substitute.For<IDataLister>();
+            var e = new Echoer(destination);
+            e.CleanDestination(null);
+        }
+
+        [Test]
+        public void CleanDestinationSourceItemsEmpty()
+        {
+            var destination = Substitute.For<IDataLister>();
+            var e = new Echoer(destination);
+            e.CleanDestination(new List<IStorageItem>());
+        }
+
+        [Test]
+        public void CleanDestinationDestinationItems()
+        {
+            var sItems = new List<IStorageItem>();
+            sItems.Add(Substitute.For<IStorageItem>());
+            var destination = Substitute.For<IDataLister>();
+            destination.List().Returns((IEnumerable<IStorageItem>)null);
+
+            var e = new Echoer(destination);
+            e.CleanDestination(sItems);
+        }
+
+        [Test]
+        public void CleanDestinationDestinationItemsEmpty()
+        {
+            var sItems = new List<IStorageItem>();
+            sItems.Add(Substitute.For<IStorageItem>());
+            var dItems = new List<IStorageItem>();
+            var destination = Substitute.For<IDataLister>();
+            destination.List().Returns(dItems);
+
+            var e = new Echoer(destination);
+            e.CleanDestination(sItems);
+        }
+
+        [Test]
+        public void CleanDestinationNothingToDelete()
+        {
+            var random = new Random();
+            var count = random.Next(1, 64);
+
+            var sItems = new List<IStorageItem>(count);
+            var dItems = new List<IStorageItem>(count);
+
+            for (var i = 0; i < count; i++)
+            {
+                var item = Substitute.For<IStorageItem>();
+                item.RelativePath.Returns(Guid.NewGuid().ToString());
+
+                sItems.Add(item);
+                dItems.Add(item);
+            }
+
+            var destination = Substitute.For<IDataLister>();
+            destination.List().Returns(dItems);
+
+            var e = new Echoer(destination);
+            e.CleanDestination(sItems);
+        }
+    }
+}
---
+++ King.Atrak/Echoer.cs
@@ -0,0 +1,65 @@
+﻿namespace King.ATrak
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Echo deletions to destination
+    /// </summary>
+    public class Echoer : IEchoer
+    {
+        #region Members
+        /// <summary>
+        /// Destination
+        /// </summary>
+        protected readonly IDataLister destination = null;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Mockable Constructor
+        /// </summary>
+        public Echoer(IDataLister destination)
+        {
+            if (null == destination)
+            {
+                throw new ArgumentNullException("destination");
+            }
+
+            this.destination = destination;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Clean Destination
+        /// </summary>
+        /// <param name="sourceItems">Source Items</param>
+        public virtual void CleanDestination(IEnumerable<IStorageItem> sourceItems)
+        {
+            if (null == sourceItems)
+            {
+                throw new ArgumentNullException("sourceItems");
+            }
+
+            if (sourceItems.Any())
+            {
+                var destinationItems = this.destination.List();
+                if (null != destinationItems && destinationItems.Any())
+                {
+                    var notFoundItems = destinationItems.Where(di => !sourceItems.Any(si => si.RelativePath == di.RelativePath));
+                    foreach (var item in notFoundItems)
+                    {
+                        //Delete Item;
+                        Trace.TraceInformation("Deleting item in destination: '{0}'.", item.RelativePath);
+                    }
+                }
+            }
+        }
+        #endregion
+    }
+}
