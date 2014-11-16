220ed77c9a7e9357beddf2fb7aef383df7f2aa68
Jef King <jef@agilebusinesscloud.com>
2014-11-15 16:43:59 -0800

    Clenlyness

--- King.Atrak/Enums.cs
+++ King.Atrak/Enums.cs
@@ -6,10 +6,25 @@
     /// </summary>
     public enum Direction : byte
     {
+        /// <summary>
+        /// Unknown
+        /// </summary>
         Unknown = 0,
+        /// <summary>
+        /// Folder to Blob
+        /// </summary>
         FolderToBlob = 1,
+        /// <summary>
+        /// Blob to Folder
+        /// </summary>
         BlobToFolder = 2,
+        /// <summary>
+        /// Blob to Blob
+        /// </summary>
         BlobToBlob = 3,
+        /// <summary>
+        /// Folder to Folder
+        /// </summary>
         FolderToFolder = 4,
     }
     #endregion
